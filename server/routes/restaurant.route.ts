79d761ec197581dfed7321e839653988da6830b5
kushal <mohitsinghkpsc@gmail.com>
2024-11-05 17:15:26 +0530

    route added for the restaurant api

--- server/routes/restaurant.route.ts
+++ server/routes/restaurant.route.ts
@@ -1,5 +1,5 @@
 import express from "express"
-import { createRestaurant, getRestaurant, updateRestaurant} from "../controller/restaurant.controller";
+import { createRestaurant, getRestaurant, getRestaurantOrder, getSingleRestaurant, searchRestaurant, updateOrderStatus, updateRestaurant} from "../controller/restaurant.controller";
 import { isAuthenticated } from "../middlewares/authorised";
 import upload from "../middlewares/multer";
 
@@ -9,6 +9,10 @@ const restauRouter = express.Router();
 restauRouter.post("/",isAuthenticated, upload.single("imageFile"), createRestaurant);
 restauRouter.get("/",isAuthenticated, getRestaurant);
 restauRouter.put("/",isAuthenticated, upload.single("imageFile"), updateRestaurant);
+restauRouter.get("/order",isAuthenticated,  getRestaurantOrder);
+restauRouter.put("/order/:orderId/status",isAuthenticated, updateOrderStatus);
+restauRouter.get("/search/:searchText",isAuthenticated, searchRestaurant);
+restauRouter.get("/:id",isAuthenticated, getSingleRestaurant);
 
 export default restauRouter;
 

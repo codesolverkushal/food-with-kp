f8e4bd84c2295e58e14a2d2f2a42c0faa130658b
kushal <mohitsinghkpsc@gmail.com>
2024-11-19 11:13:14 +0530

    configation change

--- server/index.ts
+++ server/index.ts
@@ -8,13 +8,10 @@ import cors from 'cors';
 import restauRouter from './routes/restaurant.route';
 import menuRouter from './routes/menu.route';
 import orderRoute from './routes/order.route';
-import path from 'path';
 
 dotenv.config();
 
 const app = express();
-
-const DIRNAME = path.resolve();
 
 // default middleware for any mern project
 
@@ -23,7 +20,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin: "https://food-with-kp.onrender.com",
+    origin: "http://localhost:5173",
     credentials: true
 }
 app.use(cors(corsOptions));
@@ -35,13 +32,8 @@ app.use("/api/v1/menu", menuRouter);
 app.use("/api/v1/order", orderRoute);
 
 
-app.use(express.static(path.join(DIRNAME,"/client/dist")));
-app.use("*",(_,res) => {
-    res.sendFile(path.resolve(DIRNAME, "client","dist","index.html"));
-});
 
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ||  3000;
 
 app.listen(PORT,()=>{
     connectDb();

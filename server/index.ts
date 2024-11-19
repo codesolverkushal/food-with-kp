284c1c167d8e1e8eb086f2c2556fa61dc934fec1
kushal <mohitsinghkpsc@gmail.com>
2024-11-19 11:20:58 +0530

    configration 2

--- server/index.ts
+++ server/index.ts
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true
 }
 app.use(cors(corsOptions));
